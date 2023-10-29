bdba75a6fa4a384777ae0024d5b971a2e04d932a
Glorfindel-Noldor <131824775+Glorfindel-Noldor@users.noreply.github.com>
2023-10-29 14:16:14 -0500

    Notice that we added /:id to the end of our path for our UserProfile route.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Login from "./pages/login";
+import UserProfile from "./pages/UserProfile";
+
+
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const exapmpleOf = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home />,    
+    },
+    {
+        path: '/about',
+        element: <About/>,
+    },
+    {
+        path:'/login',
+        element: <Login/>,
+    },
+    {
+        path: '/profile/:id',
+        element: <UserProfile/>,
+    },
+])
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Home />);
+root.render(<RouterProvider router={exapmpleOf}/>);
